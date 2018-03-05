5ac02c8bd9f99f622d659a0a707c10f541b922fa
emily-li <emily.li@live.co.uk>
2018-03-05 22:02:21 +0000

    Adding math functions

--- src/main/java/com/liemily/recommender/example/metrics/PairwiseDistanceCalculator.java
+++ src/main/java/com/liemily/recommender/example/metrics/PairwiseDistanceCalculator.java
@@ -15,36 +15,70 @@ public class PairwiseDistanceCalculator {
      * @return Distance matrix, D, with the same dimensions as the input matrix, X,
      * where D[i, j] expresses the distance between the ith and jth vectors of X
      */
-    public double[][] calculate(final int[][] matrix, final Metric metric) {
-        return new double[0][];
+    public double[][] calculate(final double[][] matrix, final Metric metric) {
+        switch (metric) {
+            case COSINE:
+                cosineDistance(matrix);
+            default:
+                throw new UnsupportedOperationException("Only metric type Cosine currently supported");
+        }
     }
 
-    private double[][] cosineDistance(final double[][] matrix) {
+    double[][] cosineDistance(final double[][] matrix) {
         double[][] distances = cosineSimilarity(matrix);
         Arrays.stream(distances).forEach(line -> Arrays.stream(line).forEach(i -> i = i * -1 + 1));
-        // Clip to ensure numbers lower than 0 are 0, numbers higher than 2 should be 2
         correctSelfDistances(distances);
         return distances;
     }
 
-    private double[] cosineSimilarity(final double[][] matrix) {
+    double[][] cosineSimilarity(final double[][] matrix) {
         double[][] normedMatrix = matrix.clone();
-        normalise(normedMatrix);
-        // return normedMatrix * normedMatrix.T
-        return new double[0][];
+        normedMatrix = normalise(normedMatrix);
+        double[][] transposedMatrix = transpose(normedMatrix);
+        return multiply(normedMatrix, transposedMatrix);
     }
 
-    private void correctSelfDistances(double[][] cosineDistances) {
-    }
-
-    private void normalise(double[][] matrix) {
-
-        double[] norms = getNorms(matrix);
+    double[][] transpose(final double[][] matrix) {
+        double[][] transposed = new double[matrix[0].length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] /= norms[i];
+                transposed[j][i] = matrix[i][j];
             }
         }
+        return transposed;
+    }
+
+    /**
+     * Clips matrix to ensure numbers lower than 0 are 0, numbers higher than 2 should be 2
+     *
+     * @param distances Distances to correct. Matrix of any size
+     * @return Corrected matrix
+     */
+    double[][] correctSelfDistances(final double[][] distances) {
+        final double[][] correctedDistances = new double[distances.length][distances[0].length];
+        for (int i = 0; i < distances.length; i++) {
+            for (int j = 0; j < distances[0].length; j++) {
+                double val = distances[i][j];
+                correctedDistances[i][j] = val < 0 ? 0 : val > 2 ? 2 : val;
+            }
+        }
+        return correctedDistances;
+    }
+
+    /**
+     * Normalises matrix by getting its norms and dividing the matrix by the norms
+     *
+     * @param matrix Matrix of any size
+     */
+    private double[][] normalise(final double[][] matrix) {
+        double[][] normalised = matrix.clone();
+        double[] norms = getNorms(normalised);
+        for (int i = 0; i < normalised.length; i++) {
+            for (int j = 0; j < normalised[0].length; j++) {
+                normalised[i][j] /= norms[i];
+            }
+        }
+        return normalised;
     }
 
     /**
@@ -53,22 +87,27 @@ public class PairwiseDistanceCalculator {
      * @param matrix Input square matrix
      * @return Row wise norms of the matrix
      */
-    private double[] getNorms(double[][] matrix) {
-        double[][] squares = square(matrix);
+    private double[] getNorms(final double[][] matrix) {
+        double[][] squares = multiply(matrix, matrix);
         return Arrays.stream(squares)
                 .mapToDouble(vector -> DoubleStream.of(vector).sum())
                 .map(Math::sqrt)
                 .toArray();
     }
 
-    private double[][] square(final double[][] matrix) {
-        double[][] square = new double[matrix.length][matrix[0].length];
-        Arrays.stream(square).forEach(row -> Arrays.fill(row, 0));
-        for (int i = 0; i < square.length; i++) {
-            for (int j = 0; j < square[0].length; j++) {
-                square[i][j] = matrix[i][j] * matrix[i][j];
+    double[][] multiply(final double[][] matrix1, final double[][] matrix2) {
+        double[][] multiplied = new double[matrix1.length][matrix2[0].length];
+        Arrays.stream(multiplied).forEach(row -> Arrays.fill(row, 0.0));
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                for (int k = 0; k < matrix1[0].length; k++) {
+                    multiplied[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
             }
         }
-        return square;
+        return multiplied;
     }
+
+
 }
--- src/test/java/com/liemily/recommender/example/metrics/PairwiseDistanceCalculatorTest.java
+++ src/test/java/com/liemily/recommender/example/metrics/PairwiseDistanceCalculatorTest.java
@@ -1,18 +1,82 @@
 package com.liemily.recommender.example.metrics;
 
+import com.liemily.recommender.example.cf.DataReader;
+import com.liemily.recommender.example.cf.DataSet;
+import com.liemily.recommender.example.cf.DataSetUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 public class PairwiseDistanceCalculatorTest {
     private static PairwiseDistanceCalculator pairwiseDistanceCalculator;
+    private static double[][] matrix;
+
 
     @BeforeClass
-    public static void setupBeforeClass() {
+    public static void setupBeforeClass() throws Exception {
         pairwiseDistanceCalculator = new PairwiseDistanceCalculator();
+        DataReader dataReader = new DataReader();
+        DataSetUtils dataSetUtils = new DataSetUtils();
+
+        DataSet data = dataReader.getData("testdata/header", "testdata/data", "\t");
+        String[][] ratingsMatrix = dataSetUtils.getEntityToEntityMatrix(data, "user", "item", "rating", "0.1");
+        matrix = Arrays.stream(ratingsMatrix).map(row -> Arrays.stream(row).mapToDouble(Double::parseDouble).toArray()).toArray(double[][]::new);
     }
 
     @Test
-    public void test() {
+    public void testMultiplication() {
+        final double[][] expectedMatrix = new double[][]{
+                {4.03, 0.72, 1.02, 1.32},
+                {0.72, 25.03, 1.32, 1.62},
+                {1.02, 1.32, 64.03, 1.92},
+                {1.32, 1.62, 1.92, 121.03}
+        };
+        final double[][] squaredMatrix = pairwiseDistanceCalculator.multiply(matrix, matrix);
+        assertMatrixEquals(expectedMatrix, squaredMatrix);
+    }
 
+    @Test
+    public void testTranspose() {
+        matrix = new double[][]{
+                {4.03, 0.72, 1.02},
+                {0.88, 1.22, 1.12},
+                {3.22, 1.32, 64.03}
+        };
+        final double[][] expectedMatrix = new double[][]{
+                {4.03, 0.88, 3.22},
+                {0.72, 1.22, 1.32},
+                {1.02, 1.12, 64.03}
+        };
+        final double[][] transposedMatrix = pairwiseDistanceCalculator.transpose(matrix);
+        assertMatrixEquals(expectedMatrix, transposedMatrix);
+    }
+
+    @Test
+    public void testCorrectSelfDistances() {
+        matrix = new double[][]{
+                {0, 0.01, -0.01},
+                {-1, 2, 2.1},
+                {1.99, 1.999999, 64.03}
+        };
+        final double[][] expectedMatrix = new double[][]{
+                {0, 0.01, 0},
+                {0, 2, 2},
+                {1.99, 1.999999, 2}
+        };
+        final double[][] correctedMatrix = pairwiseDistanceCalculator.correctSelfDistances(matrix);
+        assertMatrixEquals(expectedMatrix, correctedMatrix);
+    }
+
+    private void assertMatrixEquals(final double[][] expectedMatrix, final double[][] actualMatrix) {
+        assertEquals(expectedMatrix.length, actualMatrix.length);
+        assertEquals(expectedMatrix[0].length, actualMatrix[0].length);
+        for (int i = 0; i < expectedMatrix.length; i++) {
+            for (int j = 0; j < expectedMatrix[0].length; j++) {
+                assertEquals(expectedMatrix[i][j], actualMatrix[i][j], 0.00001);
+            }
+        }
     }
 }
